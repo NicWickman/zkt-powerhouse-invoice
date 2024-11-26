d04d69f2555bcb966e9f88dee75111acd5ad5239
Nic Wickman <nicwickman@gmail.com>
2024-11-26 06:28:05 -0800

    ..

---
+++ document-models/invoice/src/reducers/items.ts
@@ -0,0 +1,22 @@
+/**
+* This is a scaffold file meant for customization: 
+* - modify it by implementing the reducer functions
+* - delete the file and run the code generator again to have it reset
+*/
+
+import { InvoiceItemsOperations } from '../../gen/items/operations';
+
+export const reducer: InvoiceItemsOperations = {
+    addLineItemOperation(state, action, dispatch) {
+        // TODO: Implement "addLineItemOperation" reducer
+        throw new Error('Reducer "addLineItemOperation" not yet implemented');
+    },
+    editLineItemOperation(state, action, dispatch) {
+        // TODO: Implement "editLineItemOperation" reducer
+        throw new Error('Reducer "editLineItemOperation" not yet implemented');
+    },
+    deleteLineItemOperation(state, action, dispatch) {
+        // TODO: Implement "deleteLineItemOperation" reducer
+        throw new Error('Reducer "deleteLineItemOperation" not yet implemented');
+    },
+}
--- document-models/invoice/src/utils.ts
+++ document-models/invoice/src/utils.ts
@@ -1,1 +1,9 @@
-export {};
+import { InputMaybe } from '../gen';
+
+export const notNullUndefined = <T>(input: InputMaybe<T>) => {
+    if (input !== null && input !== undefined) {
+        return input as T;
+    } else {
+        return false;
+    }
+};
